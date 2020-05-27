680f770b4d31bec78eae71450de3760fcb99b9b2
Giang Tran Truong <ttgiang@tma.com.vn>
2020-05-27 16:03:30 +0700

    MessageListenerContainer

--- src/main/java/com/river/config/RabbitMQConfig.java
+++ src/main/java/com/river/config/RabbitMQConfig.java
@@ -7,11 +7,15 @@ import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.river.service.RabbitMQListner;
 
 @Configuration
 public class RabbitMQConfig {
@@ -51,4 +55,37 @@ public class RabbitMQConfig {
 		rabbitTemplate.setMessageConverter(jsonMessageConverter());
 		return rabbitTemplate;
 	}
+
+	// create MessageListenerContainer using default connection factory
+	@Bean
+	MessageListenerContainer messageListenerContainer(ConnectionFactory connectionFactory) {
+		SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
+		simpleMessageListenerContainer.setConnectionFactory(connectionFactory);
+		simpleMessageListenerContainer.setQueues(queue());
+		simpleMessageListenerContainer.setMessageListener(new RabbitMQListner());
+		return simpleMessageListenerContainer;
+
+	}
+
+	// create custom connection factory
+	/*
+	 * @Bean ConnectionFactory connectionFactory() { CachingConnectionFactory
+	 * cachingConnectionFactory = new CachingConnectionFactory("localhost");
+	 * cachingConnectionFactory.setUsername(username);
+	 * cachingConnectionFactory.setUsername(password); return
+	 * cachingConnectionFactory; }
+	 */
+
+	// create MessageListenerContainer using custom connection factory
+	/*
+	 * @Bean MessageListenerContainer messageListenerContainer() {
+	 * SimpleMessageListenerContainer simpleMessageListenerContainer = new
+	 * SimpleMessageListenerContainer();
+	 * simpleMessageListenerContainer.setConnectionFactory(connectionFactory());
+	 * simpleMessageListenerContainer.setQueues(queue());
+	 * simpleMessageListenerContainer.setMessageListener(new RabbitMQListner());
+	 * return simpleMessageListenerContainer;
+	 * 
+	 * }
+	 */
 }
